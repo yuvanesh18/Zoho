d48efd7bba901bb8dcb3c22fa35b7f0f7fc83731
Yuvanesh <yuvaneshnagu@gmail.com>
2024-07-12 11:47:38 +0530

    SNAKE GAME (RECURSIVE AND CONSTANT)

---
+++ src/main/java/yuvi/zoho_prep/Zoho_prep.java
@@ -0,0 +1,12 @@
+
+package yuvi.zoho_prep;
+
+public class Zoho_prep{
+
+    public static void main(String[] args) {
+        
+        Snake snake = new Snake(6,6);
+        snake.snakemove(0,0);
+        
+    }
+}
